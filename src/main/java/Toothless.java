5e442c06ab92ed8f8ea9539dbdeff5bf20be4603
Jeremy Lai <86363639+MinionWolf@users.noreply.github.com>
2025-08-27 00:32:18 +0800

    echo and list
    can echo and list whatever that is added

--- src/main/java/Toothless.java
+++ src/main/java/Toothless.java
@@ -1,6 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Toothless {
+    private static String[] list = new String[100];
+    private static int listCount = 0;
+    private static String reply;
+
     public static void main(String[] args) {
-        String logo = "Hello I'm Toothless";
+        String logo = "--------------------------------\n" +
+                "Hello I'm Toothless \n" + "What can I do for you? \n" +
+                "--------------------------------";
         System.out.println(logo);
+
+        Scanner input = new Scanner(System.in);
+
+        reply = input.nextLine();
+
+        while(!reply.equals("bye")){
+
+            if(reply.equals("list")){
+                getList(Arrays.copyOf(list, listCount));
+            }else{
+                addList(reply);
+            }
+            reply = input.nextLine();
+        }
+
+        System.out.println("--------------------------------\n" +
+                "Bye.Hope to see you again soon! \n" +
+                "--------------------------------\n");
+    }
+
+    public static void addList(String reply){
+        System.out.println("--------------------------------\n" +
+                "added: " + reply + "\n" +
+                "--------------------------------\n");
+        list[listCount] = reply;
+        listCount++;
+    }
+
+    public static void getList(String[] list){
+        System.out.println("--------------------------------");
+        for(int i = 0; i < list.length; i++){
+            System.out.println(i+1 + ". " + list[i]);
+        }
+        System.out.println("--------------------------------\n");
     }
 }
